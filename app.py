6c293a2414d74ab05c3b0d6b7f91d36013d33a0c
Phinehas Sersah <83701551+PhinehasSersah@users.noreply.github.com>
2022-05-21 12:14:37 +0000

    implemented user input into into database

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -7,16 +7,28 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:hydrocarbons@localhost:5432/alx'
 db = SQLAlchemy(app)
 
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
-    
+
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
+
 db.create_all()
-    # db.session.commit()
+# db.session.commit()
+
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    description = request.form.get('description', '')
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
 
 @app.route('/')
 def index():
