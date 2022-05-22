b6c7367b83adef544164f007f62849ae26cefc0b
Phinehas Sersah <83701551+PhinehasSersah@users.noreply.github.com>
2022-05-22 13:50:43 +0000

    implemented delete functionality

--- app.py
+++ app.py
@@ -62,6 +62,19 @@ def set_completed_todo(todo_id):
     return redirect(url_for('index'))
 
 
+@app.route('/todos/<todo_id>/delete', methods=['DELETE'])
+def delete_todo_item(todo_id):
+    try:
+        todo = Todo.query.get(todo_id)
+        db.session.delete(todo)
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
+
 @app.route('/')
 def index():
     return render_template('index.html', data=Todo.query.order_by('id').all())
