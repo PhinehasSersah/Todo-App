13119cf12047739767ccfa3479e407d4de8d7294
Phinehas Sersah <83701551+PhinehasSersah@users.noreply.github.com>
2022-05-22 09:29:15 +0000

    implemented updating completed state of the application

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, redirect, request, url_for, jsonify
+from email.policy import default
+from flask import Flask, render_template, redirect, request, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+import sys
 
 
 app = Flask(__name__)
@@ -12,8 +14,9 @@ class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
-    # debugging purposes
+    completed = db.Column(db.Boolean)
 
+    # debugging purposes
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
@@ -24,15 +27,41 @@ db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.get_json()['description']
-    todo = Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    return jsonify({
-        'description': todo.description
-    })
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
+
+
+@app.route('/todos/<todo_id>/item-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', data=Todo.query.order_by('id').all())
